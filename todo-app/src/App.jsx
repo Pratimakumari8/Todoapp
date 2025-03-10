04fe3f143c5f2924603ee2f260c6293b9ce1522a
Pratima Kumari <kumaripratima337@gmail.com>
2025-03-10 13:08:32 +0530

    modifications

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
+import { Header } from './components/Header'
+import { Tabs } from './components/Tabs'
+import { TodoList } from './components/TodoList'
+import { TodoCard } from './components/TodoCard'
 
 function App() {
-  const [count, setCount] = useState(0)
+ const todos = [
+  {input : 'Hello! Add your first todo!' , complete:true},
+  {input : 'Get the groceries!' , complete: false},
+  {input : 'Learn how to web design' , complete: false},
+  {input : 'Say hi to gran gran ' , complete: true},
+ ]
 
   return (
+
     <>
-        
+     <Header todos = {todos}/>   
+     <Tabs todos = {todos}/>
+     <TodoList todos = {todos} />
+     <TodoCard />
     </>
   )
 }
